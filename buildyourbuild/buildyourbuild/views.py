5bc3d2f5ccb2ebeab347ced4d0150fafd3d81564
Leonardo Moreira <leonardoluccas0@gmail.com>
2026-02-04 21:04:43 -0300

    Pequenas Alterações Teste

--- buildyourbuild/buildyourbuild/views.py
+++ buildyourbuild/buildyourbuild/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
+def personagem(request):
+    return render(request, 'personagem.html')
