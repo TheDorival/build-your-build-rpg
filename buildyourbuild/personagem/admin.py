6e512647c861f63de29cfbcf37347743a43d7ce7
TheDorival <leonardoluccas0@gmail.com>
2026-02-10 15:20:53 -0300

    feat: Subindo Página de Personagens Individuais

--- buildyourbuild/personagem/admin.py
+++ buildyourbuild/personagem/admin.py
@@ -3,4 +3,24 @@ from . models import Personagens
 
 # Register your models here.
 
-admin.site.register(Personagens)
+@admin.register(Personagens)
+class PersonagemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'updated']
+    list_filter = ['date', 'updated']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ['date', 'updated']
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('title', 'slug', 'body')
+        }),
+        ('Mídia', {
+            'fields': ('banner',),
+            'description': 'Upload de imagem para o banner do personagem (opcional)'
+        }),
+        ('Metadados', {
+            'fields': ('date', 'updated'),
+            'classes': ('collapse',)
+        }),
+    )
--- buildyourbuild/personagem/models.py
+++ buildyourbuild/personagem/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,10 +9,30 @@ class Personagens(models.Model):
         verbose_name = 'Personagem'
         verbose_name_plural = 'Personagens'
 
-    title = models.CharField(max_length=75)
-    body = models.TextField()
+    title = models.CharField(max_length=75, verbose_name="Nome do Personagem")
+    body = models.TextField(verbose_name="Descrição")
     slug = models.SlugField()
-    date = models.DateTimeField(auto_now_add= True)
+    date = models.DateTimeField(auto_now_add= True, verbose_name="Data de Criação")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Última Atualização")
+    sistema = models.CharField(max_length=100, blank=True, null=True, verbose_name="Sistema de RPG")
+    nivel = models.IntegerField(default=1, verbose_name="Nível")
+    classe = models.CharField(max_length=100, blank=True, null=True, verbose_name="Classe")
+    raca = models.CharField(max_length=100, blank=True, null=True, verbose_name="Raça")
+    experiencia = models.IntegerField(default=0, verbose_name="XP")
+    
+    banner = models.ImageField(
+        upload_to='personagens/banners/',
+        blank=True,
+        null=True,
+        verbose_name="Banner do Personagem",
+        help_text="Imagem de banner (opcional). Recomendado: 1200x400px"
+    )
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        # Gera slug automaticamente se não existir
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
