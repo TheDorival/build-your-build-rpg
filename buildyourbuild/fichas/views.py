c702bf0346aace94ea0ed4b2d866cfbcf85c97e3
Leonardo Moreira <leonardoluccas0@gmail.com>
2026-05-13 21:24:37 -0300

    fix: ajusta import personagem na view fichas

--- buildyourbuild/fichas/views.py
+++ buildyourbuild/fichas/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from . models import Personagens
+from . models import Personagem
 
 # Create your views here.
 def index(request):
-    personagens = Personagens.objects.all().order_by('-date')
+    personagens = Personagem.objects.all().order_by('-date')
     return render(request, 'personagem/index.html', { 'personagens': personagens})
 
 def personagem(request, slug):
-    personagem = Personagens.objects.get(slug=slug)
+    personagem = Personagem.objects.get(slug=slug)
     return render(request, 'personagem/personagem.html', { 'personagem': personagem})
